a1a8cc46a36e2b6f5961fa33fc63ce919d2279aa
unknown <rouchashoori@gmail.com>
2025-02-06 13:50:45 +0330

    function to create csv file and some other useful functions

--- helpers/tools.py
+++ helpers/tools.py
@@ -3,7 +3,7 @@ import re
 from dotenv import load_dotenv
 from telethon.tl.functions.channels import GetChannelRecommendationsRequest
 from emoji import replace_emoji
-from db import queries
+import csv
 
 # opening a file to read the channels' username
 def GetChannelsId(fileName: str):
@@ -32,15 +32,6 @@ async def GetSimilarChannels(client, channel_id: str):
 
     result = await client(GetChannelRecommendationsRequest(channel_id))
 
-# a query to insert data to database
-async def add_product(dbInstance, *args):
-    data = await dbInstance.execute_query("""
-        INSERT INTO products(price_toman, channel, message_id, details, post_link) VALUES(
-                $1,$2,$3,$4,$5           
-            ) RETURNING id
-    """,
-    *args)
-    return data
 
 # remove emojis and : in a string
 def clean_text(text):
@@ -61,7 +52,6 @@ def convert_numbers(text: str):
     # creating a translation table to translate strings
     translator     = str.maketrans(persian_characters, english_characters)
     return clean_text(text.translate(translator))
-
 
 
 
@@ -86,22 +76,42 @@ def make_post_link(channel_username, message_id) -> str:
     return f"https://t.me/{channel_username}/{message_id}"
 
 
+def human_readable_time(timestamp):
+    human_readable = timestamp.strftime("%A, %B %d, %Y at %I:%M %p")
 
-async def extract_website_address(dbInstance, text, productId):
-    urlRegex = r"(?:https?:\/\/)?(?!t\.me\b)(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)?"
-
-    urls = re.findall(urlRegex, text, re.IGNORECASE)
-
-    for url in urls:
-        websiteId = await dbInstance.execute_query(queries.insert_to_website_query, url)
-        await dbInstance.execute_query(queries.insert_to_website_junction_query, productId, websiteId)
+    return human_readable
 
 
-async def extract_phone_numbers(dbInstance, text, productId):
-    phoneRegex =  r"(?:\+98|0098|0)?(?:9\d{9}|(?:21|26|25|24|23|28|31|34|35|38|41|44|74|45|51|84|54|56|58|61|66|71|76|13|77|81|83|86|87|11|17)[-_]?\d{8})"
+def create_csv_file(products):
+    try:
+        with open("report.csv", "w+", newline='', encoding="utf-8-sig") as csvfile:
+            fieldnames = ["date", "channel", "details", "price", "post link", "other links", "phone numbers"]
+            writer = csv.writer(csvfile)
+            writer.writerow(fieldnames)
+            
+            for product in products:
+                writer.writerow([
+                    human_readable_time(product["time"]),  
+                    f'@{product["channel"]}',  
+                    product["details"],  
+                    f'{product["price_toman"]:,}',  
+                    product["post_link"],  
+                    product["website_links"], 
+                    product["phone_numbers"]
+            ])
+            return csvfile.name
+    except:
+        return False
 
-    phone_numbers = re.findall(phoneRegex, text)
 
-    for phone_number in phone_numbers:
-        phoneNumberId = await dbInstance.execute_query(queries.insert_to_phone_numbers_query, phone_number)
-        await dbInstance.execute_query(queries.insert_to_phone_numbers_junction_query, productId, phoneNumberId)
+
+def is_date(text):
+    date_regex = r'^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$'
+
+    if re.match(date_regex, text):
+        return True
+    
+    return False
+
+def generate_error(value):
+    return f"not found any rows by value {value}"
